96767cd896f60f33068be73ec3a4c818e3257438
Vizziona <138292424+vizziona@users.noreply.github.com>
2024-02-09 02:31:32 +0200

    Navbar.jsx

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 	return (
 		<Flex justifyContent={"space-between"} py={6} alignItems={"center"}>
 			<Box position={"relative"} aspectRatio={5 / 3} minHeight={20}>
-				<Image src={"/logo.png"} fill alt='github logo' sx={{ filter: "invert(1)" }} />
+				<Image src={"/vizziona.png"} fill alt='github logo' sx={{ filter: "invert(1)" }} />
 			</Box>
 			<Box>
 				<Button size='md' colorScheme='whatsapp' onClick={onOpen}>
